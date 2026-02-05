9fb72335598578c6bd8e51a3b1998ad47025e5f6
sjparkstar <sjpark@rsupport.com>
2026-02-05 11:19:26 +0900

    1

--- app/components/SimpleRoadmapBoard.tsx
+++ app/components/SimpleRoadmapBoard.tsx
@@ -740,7 +740,9 @@ export default function SimpleRoadmapBoard() {
                                 )}
                                 <span>등록: {new Date(version.created_on).toLocaleDateString('ko-KR')}</span>
                                 <a
-                                  href={`https://projects.rsupport.com/projects/${version.project?.identifier}/roadmap`}
+                                  href={`https://projects.rsupport.com/projects/${version.project?.id}/roadmap`}
+                                  /* href={`https://projects.rsupport.com/projects/${version.project?.identifier}/roadmap`} */
+                                 
                                   target="_blank"
                                   rel="noopener noreferrer"
                                   className="text-blue-600 hover:text-blue-800"
